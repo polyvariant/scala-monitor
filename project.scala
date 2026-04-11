fa01f8197dadccbe4a2656a626245a7befbd8996
Michal Pawlik <admin@michalp.net>
2026-04-11 21:03:50 +0200

    workflows on any branch

--- project.scala
+++ project.scala
@@ -2,6 +2,5 @@
 //> using platform native
 //> using nativeVersion 0.5.10
 //> using nativeMode release-fast
-//> using nativeLto thin
 //> using option -no-indent
 //> using dep com.lihaoyi::mainargs_native0.5:0.7.8
