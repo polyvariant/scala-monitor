1f87cfb8f7f29dc4aa77f44915dd3f398cff3359
Michal Pawlik <admin@michalp.net>
2026-04-14 19:04:21 +0200

    publish to sonatype

--- project.scala
+++ project.scala
@@ -5,3 +5,14 @@
 //> using option -no-indent
 //> using dep com.lihaoyi::mainargs_native0.5:0.7.8
 //> using test.dep org.scalameta::munit_native0.5:1.3.0
+//> using publish.organization org.polyvariant
+//> using publish.name scala-monitor_native0.5_3
+//> using publish.url https://github.com/polyvariant/scala-monitor
+//> using publish.license Apache-2.0
+//> using publish.vcs github:polyvariant/scala-monitor
+//> using publish.description "CLI utility that discovers running Scala/JVM-related processes"
+//> using publish.developer "majk-p|Michal Pawlik|https://github.com/majk-p"
+//> using publish.repository central-s01
+//> using publish.secretKey env:PGP_SECRET
+//> using publish.secretKeyPassword env:PGP_PASSPHRASE
+//> using publish.computeVersion git:dynver
